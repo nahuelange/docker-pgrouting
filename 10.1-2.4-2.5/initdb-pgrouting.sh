4f8fd7c115adda5ea1e15cd531f3bec20d0dec6d
Nahuel ANGELINETTI <nahuel.angelinetti@makina-corpus.com>
2018-10-04 14:25:29 +0200

    Create extension only if not exists

--- 10.1-2.4-2.5/initdb-pgrouting.sh
+++ 10.1-2.4-2.5/initdb-pgrouting.sh
@@ -6,6 +6,6 @@ export PGUSER=postgres
 
 # Add pgRouting Functions to the database
 psql --dbname="$POSTGRES_DB" <<EOSQL
-CREATE EXTENSION postgis;
-CREATE EXTENSION pgrouting;
+CREATE EXTENSION postgis IF NOT EXISTS pgrouting;
+CREATE EXTENSION pgrouting IF NOT EXISTS pgrouting;
 EOSQL
